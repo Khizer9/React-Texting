517b2b8c1493dd0fa3c3ff407eb3cb355ce4ca09
Khizer Ali <khizer358@gmail.com>
2023-06-08 00:19:46 +0500

    add logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,21 +5,42 @@ export default function TextForm(props) {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+    const handleClearClick = () => {
+        let newText = ""
+        setText(newText)
+    }
 
     const handleChange = (event) => {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState('Enter Text here');
+    const [text, setText] = useState('');
 
 
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary" onClick={handleClearClick}>Clear</button>
     </div>
+
+    <div className="container my-3">
+      <h2>Your Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read.</p>
+
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }

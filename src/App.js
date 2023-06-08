4589d01416dd7081d50b17d1393c9556dbdbf413
Khizer Ali <Khizer.Ali@Amanra-Soft.com>
2023-06-08 14:23:31 +0500

    adding dismissal alert

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
 
   const [mode, setMode] = useState ('light')
+  const [alert, setAlert] = useState (null)
+
+  const showAlert = (messege, type) => {
+    setAlert({
+      msg: messege,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
 
   const toggleMode = () => {
     if(mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'grey'
+      showAlert("Dark mode has been enabled", "success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
     }
   }
 
   return (
    <>
     <Navbar title="TextingText" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-
+    <Alert alert={alert}/>
     <div className="container my-3">
       {/* <About/> */}
-      <TextForm heading="Enter the text to analyze below" mode={mode}/>
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
     </div>
    </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,19 +4,23 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase()
         setText(newText)
+        props.showAlert("Converted to Uppercase!", "success")
     }
     const handleLoClick = () => {
         let newText = text.toLowerCase()
         setText(newText)
+        props.showAlert("Converted to Lowercase!", "success")
     }
     const handleClearClick = () => {
         let newText = ""
         setText(newText)
+        props.showAlert("Text Cleared", "success")
     }
     const handleCopyText = () => {
         let text = document.getElementById("myBox")
         text.select()
         navigator.clipboard.writeText(text.value)
+        props.showAlert("Copy to Clipboard", "success")
     }
 
     const handleChange = (event) => {
